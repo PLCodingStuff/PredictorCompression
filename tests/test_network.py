b0043516f76790bfc289254cd1d582162bb0c2e8
PLCodingStuff <plproton47@proton.me>
2026-05-09 11:29:42 +0300

    Modified 'test_network.py' code for readability.

--- tests/test_network.py
+++ tests/test_network.py
@@ -6,19 +6,6 @@ import errno
 
 
 class TestServer(unittest.TestCase):
-    @patch("socket.socket.accept")
-    def test_start_successful(self, mock_accept):
-        conn_s = socket(AF_INET, SOCK_STREAM)
-        mock_accept.return_value = (conn_s, "127.0.0.1")
-
-        conn: Connection = Connection()
-        server: Server = Server("127.0.0.1", 6000, conn)
-
-        server.start()
-
-        self.assertEqual(server.conn_s, conn_s)
-        server.close()
-
     @patch("socket.socket.bind", side_effect=OSError(errno.EADDRINUSE))
     def test_start_fail_bind_eaddrinuse(self, mock_bind):
         with self.assertRaises(OSError) as context:
@@ -38,6 +25,20 @@ class TestServer(unittest.TestCase):
             server.start()
 
         self.assertEqual(str(context.exception), str(errno.EACCES))
+
+    @patch("socket.socket.accept")
+    def test_accept_successful(self, mock_accept):
+        conn_s = socket(AF_INET, SOCK_STREAM)
+        mock_accept.return_value = (conn_s, "127.0.0.1")
+
+        conn: Connection = Connection()
+        server: Server = Server("127.0.0.1", 6000, conn)
+
+        server.start()
+
+        self.assertEqual(server.conn_s, conn_s)
+        server.close()
+
 
 if __name__ == "__main__":
     unittest.main()
