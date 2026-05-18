da0b35d2a344c979fcf2cf427874d1737273aa76
PLCodingStuff <plproton47@proton.me>
2026-05-18 18:15:27 +0300

    Created custom error for connection loss. Created Socket factory class for creating and configuring the default TCP socket. Created ServerConfig dataclass with parameters for the server and validation. Modified socket creation to entering context management, not creating ServerSocketManager. Modified Error handling in ServerSocketManager.get_message. Removed accept and message logic from ServerManager. ServerManager is now the Observer. Only executes application server code.

--- src/network/server.py
+++ src/network/server.py
@@ -4,13 +4,16 @@ from socket import (
     SOL_SOCKET,
     SO_REUSEADDR,
     SHUT_RDWR,
-    # AF_INET,
-    # SOCK_STREAM,
+    AF_INET,
+    SOCK_STREAM,
 )
+from typing import Callable, ParamSpec, TypeAlias
+from dataclasses import dataclass
+import errno
+from threading import Event
 from ipaddress import ip_address
 from src.interfaces.observer import Observer
 from src.network_components.connection import Connection
-import errno
 
 CONNECTION_LOST_ERRORS = {
     errno.ECONNRESET,
@@ -19,64 +22,78 @@ CONNECTION_LOST_ERRORS = {
     errno.ETIMEDOUT,
 }
 
+P = ParamSpec("P")
+SocketFactory: TypeAlias = Callable[P, socket]
+
+
+class ConnectionLostError(OSError):
+    """Raised when the client disconnects suddenly."""
+
+    ...
+
 
 class ServerTerminatingError(OSError):
     """Raised when the server fails to establish a connection after all retries."""
 
     def __init__(self, retries: int):
-        self.retries = retries
         super().__init__(
             f"No connection established after {retries} retries. Terminating..."
         )
 
 
-class ServerSocketManager(Observer):
-    def __init__(
-        self,
-        host: str,
-        port: int,
-        sock: socket,
-        timeout: float = 5.0,
-        retries: int = 3,
-        buffer_size: int = 1024,
-    ) -> None:
-        if not sock:
-            raise ValueError("No socket provided")
+def default_socket_factory(timeout: int) -> socket:
 
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.settimeout(timeout)
+    return sock
+
+
+@dataclass
+class ServerConfig:
+    host: str
+    port: int
+    timeout: float = 5.0
+    buffer_size: int = 1024
+    retries: int = 5
+
+    def __post_init__(self):
         try:
-            ip_address(host)
+            ip_address(self.host)
         except ValueError:
             raise ValueError("Invalid host name")
 
-        if port <= 0:
+        if self.port <= 0:
             raise ValueError("Port value out of range")
 
-        if timeout < 0.0:
+        if self.timeout < 0.0:
             raise ValueError("Timeout value out of range")
 
-        if retries < 0:
+        if self.retries < 0:
             raise ValueError("Retries value out of range")
 
-        self._retries = retries
-        self._host = host
-        self._port = port
-        self._buffer_size = buffer_size
+
+class ServerSocketManager:
+    def __init__(
+        self,
+        config: ServerConfig,
+        socket_factory: SocketFactory = default_socket_factory,
+    ) -> None:
         self._conn_s: socket | None = None
-        self._socket: socket = sock
-        self._should_close = False
-        # self._socket = socket(AF_INET, SOCK_STREAM)
-        self._socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self._socket.settimeout(timeout)
+        self._config: ServerConfig = config
+        self._socket_factory: SocketFactory = socket_factory
 
     def __enter__(self) -> "ServerSocketManager":
         try:
+            self._sock: socket = self._socket_factory(self._config.timeout)
+
             self._bind_and_listen()
-            print(f"Server listening on {self._host}:{self._port}")
+            # print(f"Server listening on {self._config.host}:{self._config.port}")
             return self
         except OSError as e:
             self._close_server_socket()
             if e.errno == errno.EACCES:
-                raise PermissionError(f"Port {self._port} is occupied")
+                raise PermissionError(f"Port {self._config.port} is occupied")
             raise
 
     def __exit__(self, exc_type, exc, tb) -> bool:
@@ -94,62 +111,62 @@ class ServerSocketManager(Observer):
             self._conn_s = None
 
     def _close_server_socket(self) -> None:
-        if self._socket:
-            self._socket.close()
-            self._socket = None
+        if self._sock:
+            self._sock.close()
+            self._sock = None
 
     def _bind_and_listen(self) -> None:
-        self._socket.bind((self._host, self._port))
-        self._socket.listen(1)
+        self._sock.bind((self._config.host, self._config.port))
+        self._sock.listen(1)
 
     def accept(self) -> str:
-        for r in range(self._retries):
+        for _ in range(self._config.retries):
             try:
-                self._conn_s, addr = self._socket.accept()
+                self._conn_s, addr = self._sock.accept()
                 return str(addr)
             except timeout:
-                # if r != self._retries - 1:
-                #     print("No connection. Retrying...")
-                ...
-
-        raise ServerTerminatingError(self._retries)
-
-    def get_message(self) -> bytearray:
-        message: bytearray = self._conn_s.recv(self._buffer_size)
-
-        if not message:
-            raise ConnectionResetError("Peer disconnected gracefully.")
-
-        return message
-
-    @property
-    def should_close(self) -> bool:
-        return self._should_close
-
-    def update(self, data: Connection) -> None:
-        if not data.state:
-            self._should_close = True
-
-
-class ServerManager:
-    def __init__(self, server_sock_man: ServerSocketManager, conn: Connection) -> None:
-        self._server_sock_man: ServerSocketManager = server_sock_man
-        self._conn: Connection = conn
-
-    def accept(self) -> None:
-        addr: str = self._server_sock_man.accept()
-        print(f"Client {addr} successfully connected")
-        self._conn.update_state()
+                continue
+        raise ServerTerminatingError(self._config.retries)
 
     def get_message(self) -> bytearray:
         try:
-            msg: bytearray = self._server_sock_man.get_message()
-            return msg
-        except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
-            self._conn.update_state()
-            return bytearray()
+            message: bytearray = self._conn_s.recv(self._config.buffer_size)
+
+            if not message:
+                raise ConnectionResetError("Peer disconnected gracefully.")
+
+            return message
         except OSError as e:
             if e.errno in CONNECTION_LOST_ERRORS:
-                self._conn.update_state()
-                return bytearray()
+                raise ConnectionLostError(str(e))
             raise
+
+
+class ServerManager(Observer):
+    def __init__(
+        self, server_sock_man: ServerSocketManager, conn: Connection, stop_event: Event
+    ) -> None:
+        self._sock_man: ServerSocketManager = server_sock_man
+        self._conn: Connection = conn
+        self._stop_event = stop_event
+
+    def update(self, data: Connection) -> None:
+        if not data.state:
+            self._stop_event.set()
+
+    def run(self) -> None:
+        with self._sock_man as server:
+            try:
+                server.accept()
+            except ServerTerminatingError:
+                self._conn.update_state()
+                return
+
+            while not self._stop_event.is_set():
+                try:
+                    msg = server.get_message()
+                    msg
+                    # handle msg
+                except ConnectionLostError:
+                    self._conn.update_state()
+                    
