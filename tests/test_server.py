3432862d9bf2b500020687ebc52be6bb5c2ee2d3
PLCodingStuff <plproton47@proton.me>
2026-05-19 14:59:12 +0300

    Added test for ServerSocketManager.accept

--- tests/test_server.py
+++ tests/test_server.py
@@ -58,9 +58,35 @@ def test_server_socket_manager_enter_exit():
     with server_sock_man:
         pass
 
+    # Server socket setup
     assert mock_sock.setsockopt.called_once()
     assert mock_sock.settimeout.called_once()
+
+    # Socket changed states
     assert mock_sock.bind.called_once()
     assert mock_sock.listen.called_once()
+
+    # Socket closed
     assert mock_sock.close.called_once()
+
+    # Accept was not called
     assert not mock_sock.accept.called
+
+def test_server_socket_manager_accept():
+    host: str = "127.0.0.1"
+    port: int = 6000
+
+    conf: ServerSocketConfig = ServerSocketConfig(host, port)
+
+    mock_sock: MagicMock = MagicMock()
+    mock_client_sock: MagicMock = MagicMock()
+    mock_sock.accept.return_value = (mock_client_sock, None)
+
+    server_sock_man: ServerSocketManager = ServerSocketManager(conf, mock_sock)
+
+    client_sock = None
+    with server_sock_man as sock:
+        client_sock = sock.accept()
+
+    assert mock_sock.accept.called
+    assert client_sock == mock_client_sock
