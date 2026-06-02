2b6b887ecb7585c92f5a3e1f33d11cd07b3eb791
PLCodingStuff <plproton47@proton.me>
2026-06-02 20:26:53 +0300

    Small refactoring at main.py. Added new Node() object.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from sys import argv
 from src.network.node import Node
 
 
-
 def get_addresses(filename: str) -> tuple[str, int, str, int]:
     """
     Parse the JSON file to extract server and peer addresses and ports.
@@ -26,20 +25,23 @@ def get_addresses(filename: str) -> tuple[str, int, str, int]:
         raise FileNotFoundError("Invalid suffix.")
 
     with open(filename) as js:
-        addressess = load(js)
-        if "address" in addressess:
-            address = addressess["address"]
-        if "peer_address" in addressess:
-            peer_address = addressess["peer_address"]
-        if "port" not in addressess:
+        config = load(js)
+
+        if "address" in config:
+            address = config["address"]
+        if "port" not in config:
             raise ValueError("Port is not provided in JSON file.")
-        if "peer_port" not in addressess:
+        if "peer_address" in config:
+            peer_address = config["peer_address"]
+        if "peer_port" not in config:
             raise ValueError("Peer port is not provided in JSON file.")
 
-        port = addressess["port"]
-        peer_port = addressess["peer_port"]
-
-    return address, port, peer_address, peer_port
+        return (
+            address,
+            config["port"],
+            peer_address,
+            config["peer_port"],
+        )
 
 
 def main():
@@ -53,18 +55,21 @@ def main():
     try:
         if len(argv) != 2:
             raise ValueError("Invalid number of command line arguments")
-        host, port, peer_host, peer_port = get_addresses(argv[1])
 
-        node = Node(host, port, peer_host, peer_port)
-        node.start_chat()
+        json_file = argv[1]
+        host, port, peer_host, peer_port = get_addresses(json_file)
     except ValueError as e:
-        print(f"Error while loading: {e}")
+        print(f"Error while loading: {str(e)}")
+        print("Terminating Process")
+        return
     except FileNotFoundError as e:
-        print(f"Error while loading: {e}")
-    print("Terminating Process")
+        print(f"Error while loading: {str(e)}")
+        print("Terminating Process")
+        return
+
+    node = Node(host, port, peer_host, peer_port)
+    node.start_chat()
 
 
 if __name__ == "__main__":
-    ...
-
-        
+    main()
