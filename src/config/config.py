9986024c301fcd093f7c69703c41905a1aa45768
PLCodingStuff <plproton47@proton.me>
2026-06-29 11:43:10 +0300

    Removed 'get_address()' function from src/config/config.py. Added 'read_config_json()' function.

--- src/config/config.py
+++ src/config/config.py
@@ -1,40 +1,23 @@
 from json import load
 
-def get_addresses(filename: str) -> tuple[str, int, str, int]:
-    """
-    Parse the JSON file to extract server and peer addresses and ports.
 
-    Args:
-        filename (str): The name of the JSON file containing address configuration.
-
-    Returns:
-        tuple: A tuple containing the host address, port, peer address, and peer port.
-
-    Raises:
-        FileNotFoundError: If the file doesn't end with .json or the file is not found.
-        ValueError: If port or peer_port is missing in the JSON file.
-    """
-    address: str = "127.0.0.1"
-    peer_address: str = "127.0.0.1"
-
+def read_config_json(filename: str) -> dict:
     if not filename.endswith(".json"):
         raise FileNotFoundError("Invalid suffix.")
 
     with open(filename) as js:
         config = load(js)
 
-        if "address" in config:
-            address = config["address"]
-        if "port" not in config:
-            raise ValueError("Port is not provided in JSON file.")
-        if "peer_address" in config:
-            peer_address = config["peer_address"]
-        if "peer_port" not in config:
-            raise ValueError("Peer port is not provided in JSON file.")
+    if "port" not in config:
+        raise ValueError("Port is not provided in configuration file.")
 
-        return (
-            address,
-            config["port"],
-            peer_address,
-            config["peer_port"],
-        )
+    if "timeout" not in config:
+        config["timeout"] = 5.0
+
+    if "retires" not in config:
+        config["retries"] = 3.0
+
+    if "address" not in config:
+        config["address"] = "127.0.0.1"
+
+    return config
